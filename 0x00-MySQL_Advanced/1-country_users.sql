7ad7ec946e6b239a0d4c66323437d6d5d656e64b
Itohan <momoduitohan5@gmail.com>
2024-08-07 20:42:49 +0100

    File correction

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,8 +1,6 @@
 -- Write a SQL script that creates a table users following these requirements
 
-DROP TABLE IF EXISTS users;
 CREATE TABLE users (
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	email VARCHAR(255) NOT NULL UNIQUE,
 	name VARCHAR(255) NOT NULL
-);
