342227c4fab28641b681617e13a5462b117c1ebe
Itohan <momoduitohan5@gmail.com>
2024-08-10 22:24:40 +0100

    This is the index

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,4 +1,5 @@
 -- Write a SQL script that creates an index idx_name_first on the table names and the first letter of name
 
+
 DROP INDEX IF EXISTS idx_name_first_score ON names;
-CREATE INDEX idx_name_first_score ON names (name(1));
+CREATE INDEX idx_name_first_score ON names(name(1));
