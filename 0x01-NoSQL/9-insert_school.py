c3f7eb9a4580313fb50aed0a507964e274f95156
Itohan <momoduitohan5@gmail.com>
2024-08-13 13:31:08 +0100

    a Python function that inserts a new document in a collection based on kwargs

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -8,7 +8,7 @@ from pymongo import MongoClient
 
 
 def insert_school(mongo_collection, **kwargs):
-    """"""
+    """Returns the new _id"""
 
     result = mongo_collection.insert_one(kwargs)
     return result.inserted_id
